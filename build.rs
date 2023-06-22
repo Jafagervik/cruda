b8fc471338bcca4b0c7d542ba5c66d12f1831905
Jafagervik <jorgenafagervik@gmail.com>
2023-06-22 18:24:22 +0900

    update ptx dir in  build file

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use cuda_builder::CudaBuilder;
 
 fn main() {
     CudaBuilder::new("../rustcuda")
-        .copy_to("some/path.ptx")
+        .copy_to("ptxdir/path.ptx")
         .build()
         .unwrap();
 }
